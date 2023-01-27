8137f4d65734bc171f46be82a97d7ff4979e7d0b
Ishant <ishant0203@gmail.com>
2023-01-27 17:24:54 +0530

    added new scripts for interactables

---
+++ Assets/InteractableUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableUI : MonoBehaviour
+{
+    [SerializeField]
+    Toggle toggle;
+    [SerializeField]
+    GameObject Interactabelobject;
+    [SerializeField]
+    GameObject gameObject;
+    Vector3 initalposition;
+    Quaternion initialRotation;
+    
+    
+
+    void Start()
+    {
+        initalposition = Interactabelobject.transform.localPosition;
+        initialRotation = Interactabelobject.transform.localRotation;
+        toggle.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            if (Interactabelobject.transform.localPosition != initalposition || Interactabelobject.transform.localRotation != initialRotation)
+            {
+                toggle.interactable = true;
+            }
+        }
+        else
+        {
+            initalposition = Interactabelobject.transform.localPosition;
+            initialRotation = Interactabelobject.transform.localRotation;
+        }
+    }
+
+
+
+}
