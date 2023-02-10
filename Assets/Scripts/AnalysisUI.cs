fb3f1355652ff6886c43ddf014f10b451c3533b3
Ishant <ishant0203@gmail.com>
2023-02-10 15:08:12 +0530

    bug fixes

---
+++ Assets/Scripts/AnalysisUI.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AnalysisUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] gameObjects;
+    [SerializeField]
+    int checklist;
+    GameManager gameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.instance;
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            switch (checklist)
+            {
+                case 1:
+                    gameObjects[i].GetComponent<TextMeshPro>().text = gameManager.PreFlightCheckList[i].text;
+                    gameObjects[i].GetComponentInChildren<Toggle>().isOn = gameManager.PreFlightCheckList[i].WasTicked;
+                    break;
+                case 2:
+                    gameObjects[i].GetComponent<TextMeshPro>().text = gameManager.EngineStartCheckList[i].text;
+                    gameObjects[i].GetComponentInChildren<Toggle>().isOn = gameManager.EngineStartCheckList[i].WasTicked;
+                    break;
+                case 3:
+                    gameObjects[i].GetComponent<TextMeshPro>().text = gameManager.PreTaxiCheckList[i].text;
+                    gameObjects[i].GetComponentInChildren<Toggle>().isOn = gameManager.PreTaxiCheckList[i].WasTicked;
+                    break;
+                case 4:
+                    gameObjects[i].GetComponent<TextMeshPro>().text = gameManager.OnRunwayCheckList[i].text;
+                    gameObjects[i].GetComponentInChildren<Toggle>().isOn = gameManager.OnRunwayCheckList[i].WasTicked;
+                    break;
+                case 5:
+                    gameObjects[i].GetComponent<TextMeshPro>().text = gameManager.AfterLandingCheckList[i].text;
+                    gameObjects[i].GetComponentInChildren<Toggle>().isOn = gameManager.AfterLandingCheckList[i].WasTicked;
+                    break;
+                default:
+                    Debug.Log("choose checklist between 1 to 5");
+                    break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+}
